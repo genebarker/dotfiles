707600fce66de99e407d8d2d3870b6b124cd6fb0
Eugene F. Barker <genebarker@gmail.com>
2025-06-09 18:14:01 +1300

    Improve Super-0 app closer to close peculiar apps

--- karabiner/close-all-apps.sh
+++ karabiner/close-all-apps.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Get a list of all open, non-background apps except Finder
+#---
+# Close the non-background apps
+#---
 APP_LIST=$(osascript <<EOF
 tell application "System Events"
 	set appNames to name of every process whose background only is false
@@ -9,10 +11,28 @@ return appNames
 EOF
 )
 
-# Loop through and close each (except Finder)
 IFS=", " read -ra APPS <<< "$APP_LIST"
 for APP in "${APPS[@]}"; do
 	if [[ "$APP" != "Finder" ]]; then
 		osascript -e "tell application \"$APP\" to quit"
 	fi
+done
+
+#---
+# Close the Safari web apps
+#---
+ps -ax -o pid,command | grep "Web App.app" | grep -- "--bundleidentifier" | awk -F '--bundleidentifier ' '{print $2}' | awk '{print $1}' | while read bundleID; do
+  osascript -e "quit app id \"$bundleID\""
+done
+
+#---
+# Close known apps not caught by above checks
+#---
+STRAGGLERS=("e-Sword X")
+
+for APP in "${STRAGGLERS[@]}"; do
+  if pgrep -x "$APP" > /dev/null; then
+    echo "Closing $APP..."
+    osascript -e "tell application \"$APP\" to quit"
+  fi
 done
