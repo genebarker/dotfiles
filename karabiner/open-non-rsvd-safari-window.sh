c1ec4ad2aebd15ed67dfa5d9361a3afe9e5f236d
Eugene F. Barker <genebarker@gmail.com>
2025-06-28 23:29:50 +1300

    Add app to smart launch safari for rsvd hosts

---
+++ karabiner/open-non-rsvd-safari-window.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if ! pgrep -x "Safari" > /dev/null; then
+    open -a "Safari"
+    exit
+fi 
+
+osascript <<EOF
+set reservedHosts to {"github.com", "calendar.google.com", "voice.google.com", "youtube.com"}
+
+tell application "Safari"
+    set winList to every window
+    set foundWin to false
+    repeat with theWin in winList
+        set firstTabURL to get URL of tab 1 of theWin
+        set reservedWin to false
+        repeat with theHost in reservedHosts
+            if firstTabURL contains theHost then
+                set reservedWin to true
+                exit repeat
+            end if
+        end repeat
+        if not reservedWin then
+            set foundWin to true
+            set index of theWin to 1
+            exit repeat
+        end if
+    end repeat
+    if not foundWin then
+        make new document
+    end if
+    activate
+end tell
+EOF
---
+++ karabiner/open-rsvd-safari-window.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TARGET_HOST="$1"  # e.g. github.com or calendar.google.com
+
+if ! pgrep -x "Safari" > /dev/null; then
+    open -a "Safari" "https://$TARGET_HOST"
+    exit
+fi 
+
+osascript <<EOF
+set targetHost to "$TARGET_HOST"
+
+tell application "Safari"
+    set winList to every window
+    set foundWin to false
+    repeat with theWin in winList
+        set firstTabURL to get URL of tab 1 of theWin
+        if firstTabURL contains targetHost then
+            set foundWin to true
+            set index of theWin to 1
+            exit repeat
+        end if
+    end repeat
+    if not foundWin then
+        make new document with properties {URL:"https://" & targetHost}
+    end if
+    activate
+end tell
+EOF
