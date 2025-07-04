669fb7bb7e293db07fd7424392619a4ba20a7fa4
Eugene F. Barker <genebarker@gmail.com>
2025-07-04 20:49:46 +1300

    Change toggle so that it cycles messaging apps

--- karabiner/toggle-messaging.sh
+++ karabiner/toggle-messaging.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
-# Check which app is frontmost
+# List of messaging apps
+apps=("Messages" "Signal" "WhatsApp")
+state_file="$HOME/.config/messages.app"
+
+# Get the frontmost app
 FRONTAPP=$(osascript -e 'tell application "System Events" to get name of first process whose frontmost is true')
 
-if [ "$FRONTAPP" = "Messages" ]; then
-  open -a Signal
+# Find index of frontmost app in the list
+index=-1
+for i in "${!apps[@]}"; do
+  if [ "${apps[$i]}" = "$FRONTAPP" ]; then
+    index=$i
+    break
+  fi
+done
+
+if [ $index -ge 0 ]; then
+  # If frontmost app is in the list, switch to the next one
+  next_index=$(( (index + 1) % ${#apps[@]} ))
+  next_app="${apps[$next_index]}"
+  echo "$next_app" > "$state_file"
+  open -a "$next_app"
 else
-  open -a Messages
+  # Otherwise, open the last used messaging app or default to the first
+  if [ -f "$state_file" ]; then
+    last_app=$(cat "$state_file")
+  else
+    last_app="${apps[0]}"
+  fi
+  open -a "$last_app"
 fi
